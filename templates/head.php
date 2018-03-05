fc2aa80a42021c36ffe7ef7324d1b7ef50cc4511
Tiago Almeida <bomdia@tiansial.com>
2018-03-05 08:44:00 +0000

    fix home slider title width

--- templates/head.php
+++ templates/head.php
@@ -4,5 +4,12 @@
   <meta name="theme-color" content="#17161A" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://use.typekit.net/vel0fom.css">
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  <link rel="manifest" href="/site.webmanifest">
+  <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#f7a700">
+  <meta name="msapplication-TileColor" content="#262020">
+  <meta name="theme-color" content="#262020">
   <?php wp_head(); ?>
 </head>
